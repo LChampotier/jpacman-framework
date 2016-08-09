3e20e3f55c112aac96eb45cac5a6780126ac65ca
Champotier <paulgerard.tatou@gmail.com>
2016-08-09 18:43:23 +0200

    Aucun changement

---
+++ src/main/java/nl/tudelft/jpacman/ui/GhostPanel.java
@@ -0,0 +1,7 @@
+package nl.tudelft.jpacman.ui;
+
+/**
+ * Created by UMONS on 08/08/2016.
+ */
+public class GhostPanel {
+}
---
+++ src/main/java/nl/tudelft/jpacman/ui/MenuBar.java
@@ -0,0 +1,7 @@
+package nl.tudelft.jpacman.ui;
+
+/**
+ * Created by UMONS on 07/08/2016.
+ */
+public class MenuBar {
+}
